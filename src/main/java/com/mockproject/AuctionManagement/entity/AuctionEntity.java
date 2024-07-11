4e635800d2405a198a34ec421dcc704f6fe9b93b
Trúc Phạm <kientruc582@gmail.com>
2024-07-11 11:42:54 +0700

    feat : add HolidayEntity

--- src/main/java/com/mockproject/AuctionManagement/entity/AuctionEntity.java
+++ src/main/java/com/mockproject/AuctionManagement/entity/AuctionEntity.java
@@ -61,4 +61,8 @@ public class AuctionEntity extends AbstractEntity{
     @OneToMany(mappedBy = "auctionEntity")
     private Set<AuctionHasAssetEntity> auctionHasAssetEntities = new HashSet<>();
 
+    @ManyToOne()
+    @JoinColumn(name = "id_holiday")
+    private HolidayEntity holidayEntity;
+
 }
