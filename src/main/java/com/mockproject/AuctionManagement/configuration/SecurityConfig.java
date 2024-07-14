3b37603dae4e458138697649aa64e2a2dce9c928
phuc20057371 <foxfessor@gmail.com>
2024-07-14 14:24:26 +0700

    add feature swagger config

--- src/main/java/com/mockproject/AuctionManagement/configuration/SecurityConfig.java
+++ src/main/java/com/mockproject/AuctionManagement/configuration/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.mockproject.AuctionManagement.configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,34 +26,31 @@ import org.springframework.web.filter.CorsFilter;
 @EnableMethodSecurity
 public class SecurityConfig {
     private final String[] PUBLIC_ENDPOINTS = {
-            "/users", "/auth/*"
+            "/users", "/auth/*", "/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**"
     };
-
 
     @Autowired
     private CustomJwtDecoder customJwtDecoder;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS)
-                .permitAll()
-                .anyRequest()
-                .authenticated());
+        httpSecurity.authorizeHttpRequests(request -> request
+                .requestMatchers(PUBLIC_ENDPOINTS).permitAll()
+                .anyRequest().authenticated());
 
         httpSecurity.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtConfigurer -> jwtConfigurer
                         .decoder((JwtDecoder) customJwtDecoder)
                         .jwtAuthenticationConverter(jwtAuthenticationConverter()))
                 .authenticationEntryPoint(new JwtAuthenticationEntryPoint()));
-        httpSecurity.csrf(AbstractHttpConfigurer::disable);
 
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
         return httpSecurity.build();
     }
 
     @Bean
-    public CorsFilter corsFilter(){
+    public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-
-        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedOriginPattern("*");
         corsConfiguration.addAllowedMethod("*");
         corsConfiguration.addAllowedHeader("*");
 
@@ -75,4 +75,6 @@ public class SecurityConfig {
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(10);
     }
+
+
 }
--- src/main/java/com/mockproject/AuctionManagement/controller/AuthenticationController.java
+++ src/main/java/com/mockproject/AuctionManagement/controller/AuthenticationController.java
@@ -7,6 +7,8 @@ import com.mockproject.AuctionManagement.dto.response.AuthenticationResponse;
 import com.mockproject.AuctionManagement.dto.response.ResponseData;
 import com.mockproject.AuctionManagement.dto.response.ResponseError;
 import com.mockproject.AuctionManagement.service.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -22,16 +24,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Tag(name = "Authentication", description = "Authentication API")
 public class AuthenticationController {
 
     AuthenticationService authenticationService;
 
+    @Operation(
+            summary = "login ",
+            description = "provide email and password for user to access the system"
+    )
     @PostMapping("/login")
     ApiResponse<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
         var result = authenticationService.authenticate(request);
         return ApiResponse.<AuthenticationResponse>builder().result(result).build();
     }
-
+    @Operation(
+            summary = "register",
+            description = "register new user"
+    )
     @PostMapping("/register")
     public ResponseData<?> register(@Valid @RequestBody RegisterRequestDTO request) {
         try {
