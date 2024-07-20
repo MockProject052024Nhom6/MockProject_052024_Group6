5db57adc1cc41e3a6c36165c80a682f751082ff7
Min-Trees <2224802010382@student.tdmu.edu.vn>
2024-07-20 14:30:45 +0700

    commit auction

--- src/main/java/com/mockproject/AuctionManagement/repository/UserRepository.java
+++ src/main/java/com/mockproject/AuctionManagement/repository/UserRepository.java
@@ -19,6 +19,6 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByUsername(String username);
   
     Optional<UserEntity> findByEmail(String email);
-  
+
     Optional<UserEntity> getUserEntityByIdUser(Long idUser);
 }
